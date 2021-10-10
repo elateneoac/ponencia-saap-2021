29a204441c49f4081ec4c3d0131b412c3f32edf9
manu <m.iglesias91@gmail.com>
2021-10-10 18:29:11 -0300

    nuevo script de armar bolsas

--- scripts/03_armar_bolsas.py
+++ scripts/03_armar_bolsas.py
@@ -2,30 +2,33 @@ import re
 import datatable
 from datatable import f
 
-
 clasificadas = datatable.fread('data/40-noticias-con-candidatos-clasificadas.csv')[:,:'dimensión valorativa?']
 clasificadas.names = ('url', 'diario', 'seccion', 'sirve', 'dsustantiva', 'dvalorativa')
-clasificadas[f.sirve == True,:]
-urls = clasificadas[:, f.url].to_list()[0]
+urls = clasificadas[f.sirve == True, f.url].to_list()[0]
 
 noticias = open('data/noticias.csv', 'rt')
 noticias.readline()
+
+import csv
+import sys
+csv.field_size_limit(sys.maxsize)
+csvnoticias = csv.reader(noticias)
 
 import spacy
 nlp = spacy.load('es_core_news_md')
 
 bolsas = {}
-while True:
-    noticia = noticias.readline()
+i = 1
+# while True:
+for url, diario, seccion, fecha, titulo, texto in csvnoticias:
+    print('noticia: ' + str(i))
 
-    if noticia == '':
-        break
-
-    url = noticia.split(',', 1)[0]
-    if url not in urls:
+    if texto == '':
         continue
 
-    url, diario, seccion, fecha, titulo, texto = re.split(''',(?=(?:[^'"]|'[^']*'|"[^"]*")*$)''', noticia)
+    if url not in urls:
+        i += 1
+        continue
 
     dsustantiva, dvalorativa = clasificadas[f.url == url,(f.dsustantiva, f.dvalorativa)].to_tuples()[0]
 
@@ -35,9 +38,14 @@ while True:
     if dvalorativa not in bolsas:
         bolsas[dvalorativa] = {}
 
-    terminos = [t.lemma_.lower() for t in nlp(titulo + '.' + texto) if t.pos_ == 'NOUN' or t.pos_ == 'ADJ' or t.pos_ == 'PROPN']
+    for t in nlp(titulo + '.' + texto):
+        termino = ''
+        if t.pos_ == 'NOUN' or t.pos_ == 'ADJ':
+            termino = t.lemma_.lower()
 
-    for termino in terminos:
+        if termino == '':
+            continue
+
         if termino in bolsas[dsustantiva]:
             bolsas[dsustantiva][termino] += 1
         else:
@@ -48,12 +56,15 @@ while True:
         else:
             bolsas[dvalorativa][termino] = 1
 
+    i += 1
+
 for bolsa in bolsas.items():
     path = 'data/bolsa_' + bolsa[0] + '.csv'
-    fbolsa = open(path)
+    fbolsa = open(path, 'wt')
     fbolsa.write('termino,freq\n')
 
-    terminos = [termino for termino in bolsa[1].items()].sort(key = lambda x : x[1], reverse = True)
+    terminos = [termino for termino in bolsa[1].items() if termino[1] > 3]
+    terminos.sort(key = lambda x : x[1], reverse = True)
     for termino in terminos:
         fbolsa.write(termino[0] + ',' + str(termino[1]) + '\n')
     fbolsa.close()
